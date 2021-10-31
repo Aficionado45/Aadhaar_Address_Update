e6513494f9f1205a8fe35bb48ac9f3a8edebb7a3
PranavNair01 <n.pranav@iitg.ac.in>
2021-10-31 15:21:59 +0530

    Capture needs fix

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Aadhar Address Update",
-      initialRoute: 'welcome',
+      initialRoute: 'capture',
       routes: {
         'welcome': (context) => WelcomeScreen(),
         'oplogin': (context) => opLogin(),
